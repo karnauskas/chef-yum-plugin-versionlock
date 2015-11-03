cbd5c53c810a13491f077351fada37c8c608c350
Marius Karnauskas <marius@karnauskas.lt>
2015-11-03 22:21:12 +0000

    lint cleanup

--- README.md
+++ README.md
@@ -25,6 +25,15 @@ Include `yum-plugin-versionlock` in your node's `run_list`:
 }
 ```
 
+# Contributing
+
+1. Fork the repository on Github
+2. Create a named feature branch (like `add_component_x`)
+3. Write your change
+4. Write tests for your change (if applicable)
+5. Run the tests, ensuring they all pass
+6. Submit a Pull Request using Github
+
 ## License and Authors
 
-Author:: Marius Karnauskas (<marius@karnauskas.lt>)
+Author: Marius Karnauskas (<marius@karnauskas.lt>)
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,14 +2,28 @@
 # Cookbook Name:: yum-plugin-versionlock
 # Recipe:: default
 #
-# Copyright (C) 2015 Marius Karnauskas
+# Copyright 2015, Marius Karnauskas <marius@karnauskas.lt>
 #
-# All rights reserved - Do Not Redistribute
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 template '/etc/yum/pluginconf.d/versionlock.conf' do
   source 'versionlock.conf.erb'
-  variables config: node['yum-plugin-versionlock']
+  variables
+    'enabled': node['yum-plugin-versionlock']['enabled'],
+    'listfile': node['yum-plugin-versionlock']['listfile'],
+    'follow_obsoletes': node['yum-plugin-versionlock']['follow_obsoletes']
 end
 
 template node['yum-plugin-versionlock']['locklist'] do

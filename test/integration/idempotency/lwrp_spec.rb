c5b3153ec48c72e44389b561bebde2de6b531f07
Marius Karnauskas <1094012+karnauskas@users.noreply.github.com>
2020-09-28 16:32:17 +0100

    fix test

--- test/integration/idempotency/lwrp_spec.rb
+++ test/integration/idempotency/lwrp_spec.rb
@@ -5,7 +5,7 @@ if os["release"][0].to_i >= 8
     it { should exist }
     it { should be_file }
     its("content") { should match "rpm-0:4.11.3-21.x86_64" }
-    its("content") { should match "sed-0:4.2.2-6.x86_64" }
+    its("content") { should match "sed-0:4.2.2-5.x86_64" }
     its("content") { should_not match "yum-0:3.4.3-150.x86_64" }
   end
 else
@@ -13,7 +13,7 @@ else
     it { should exist }
     it { should be_file }
     its("content") { should match "0:rpm-4.11.3-21.x86_64" }
-    its("content") { should match "0:sed-4.2.2-6.x86_64" }
+    its("content") { should match "0:sed-4.2.2-5.x86_64" }
     its("content") { should_not match "0:yum-3.4.3-150.x86_64" }
   end
 end
